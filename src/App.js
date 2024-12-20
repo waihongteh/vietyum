d270c21bf7c082cc05cf92d31d22eef8b30cdd55
waihongteh <tehwaihong1126@gmail.com>
2024-12-20 12:51:47 +0800

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ function App() {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAosqn775NjdrNvWGX85XlDdAYsVz3QZds",
-  authDomain: "vietyum-ed058.firebaseapp.com",
-  projectId: "vietyum-ed058",
-  storageBucket: "vietyum-ed058.firebasestorage.app",
-  messagingSenderId: "877771551482",
-  appId: "1:877771551482:web:490a2d1324b055f00c0f43",
-  measurementId: "G-WFGSZHWKD8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
